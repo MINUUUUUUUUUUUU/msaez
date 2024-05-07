913e0ede2eb43a2f874ffbb7fc05b21c7dadc80a
KWON-MINWOO <117968404+MINUUUUUUUUUUUU@users.noreply.github.com>
2024-05-07 10:20:00 +0900

    commit

---
+++ InHello/src/main/java/untitled/external/WorldService.java
@@ -0,0 +1,20 @@
+package untitled.external;
+
+import java.util.Date;
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+    name = "OutHello",
+    url = "${api.url.OutHello}",
+    fallback = ServiceImpl.class
+)
+public interface WorldService {
+    @GetMapping(path = "/worlds")
+    public List<World> getWorld();
+
+    @GetMapping(path = "/worlds/{id}")
+    public World getWorld(@PathVariable("id") Long id);
+}
